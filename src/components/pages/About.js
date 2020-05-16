6a2f7777c827fe2d2956cda6a896aaa43bcb3a1b
raghavAricent <raghav.goyal@aricent.com>
2020-05-16 11:58:02 +0530

    16/05/2020

---
+++ src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+
+function About() {
+
+  return (
+
+    <React.Fragment>
+
+      <h1>About</h1>
+
+      <p>This is the TodoList app by Raghav goyal. It is the first ever react app</p>
+
+    </React.Fragment>
+
+  )
+
+}
+
+
+
+
+
+export default About;
